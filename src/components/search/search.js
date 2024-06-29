d96c68b4790f66bffaf9eb5b9cd00eab8c03e8a0
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-06-29 20:03:21 +0300

    Add function search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,10 +1,15 @@
 import './search.css';
-import { DivComponent } from '../../common/divComponent';
+import { DivComponent } from '../../common/divComponent.js';
+import { debounce } from '../../utils/debounce.js';
 
 export class Search extends DivComponent {
 	constructor(state) {
 		super();
 		this.state = state;
+	}
+
+	search() {
+		this.state.searchQuery = this.element.querySelector('input').value;
 	}
 
 	render() {
@@ -13,9 +18,11 @@ export class Search extends DivComponent {
 			<input 
 				type='text' 
 				placeholder='Find a book or author....' 
-				value='${this.state.searchQuery ?? ""}'
+				value='${this.state.searchQuery ?? ''}'
 			/>
 		`;
+		this.element.querySelector('input')
+			.addEventListener('input', debounce(this.search.bind(this), 1000));
 		return this.element;
 	}
 }
