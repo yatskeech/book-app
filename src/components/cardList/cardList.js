0c96bef2dd433e620baf8e6e91d067e03c8a3afb
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-07-02 17:31:19 +0300

    Remove title in 'cardList'

--- src/components/cardList/cardList.js
+++ src/components/cardList/cardList.js
@@ -9,39 +9,26 @@ export class CardList extends DivComponent {
 		this.state = state;
 	}
 
-	renderLoad() {
-		this.element.innerHTML = `
-				<div class='card-list__loading'>Loading...</div>	
-			`;
-		return this.element;
-	}
-
-	renderBooks(parentElement) {
-		this.state.data.docs.forEach((book) => {
-			parentElement.append(new Card(this.appState, book).render());
-		});
-	}
-
 	render() {
 		this.element.classList.add('card-list');
 		if (this.state.loading) {
-			return this.renderLoad();
-		}
-
-		if (this.state.recommended) {
 			this.element.innerHTML = `
-				<h1>Recommended books</h1>
-				<div class='card-list__grid'></div>
+				<div class='card-list__info'>Loading...</div>	
 			`;
-			this.renderBooks(this.element.querySelector('.card-list__grid'));
 			return this.element;
 		}
 
-		this.element.innerHTML = `
-			<h1>Books found - ${this.state.data.numFound}</h1>	
-			<div class='card-list__grid'></div>
-		`;
-		this.renderBooks(this.element.querySelector('.card-list__grid'));
+		if (!this.state.data.docs?.length) {
+			this.element.innerHTML = `
+				<div class='card-list__info'>No matching books</div>	
+			`;
+			return this.element;
+		}
+
+		this.state.data.docs.forEach((book) => {
+			this.element.append(new Card(this.appState, book).render());
+		});
+
 		return this.element;
 	}
 }
