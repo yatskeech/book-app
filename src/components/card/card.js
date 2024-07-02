811eca28476a5f9443e0471741a8b36c35084434
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-07-02 19:37:03 +0300

    Add link to each book

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -13,15 +13,15 @@ export class Card extends DivComponent {
 		const isFavorite = this.appState.favorites.find(x => x.key === this.cardState.key);
 		this.element.classList.add('card');
 		this.element.innerHTML = `
-			<div class='card__img-box'>
+			<a class='card__img-box' href='#book?id=${this.cardState.key.slice(7)}'>
 				<img src='https://covers.openlibrary.org/b/olid/${this.cardState.edition_key?.[0]}-M.jpg' alt=''>
-			</div>
+			</a>
 			<div class='card__content'>
-				<div class='card__content-info'>
+				<a class='card__content-info' href='#book?id=${this.cardState.key.slice(7)}'>
 					<span class='card__info-tag'>${shortString(this.cardState.subject?.slice(0, 2).join(', ') ?? 'Unknown genre', 28)}</span>
 					<h2 class='card__info-title'>${shortString(this.cardState.title, 28)}</h2>
 					<span class='card__info-author'>${shortString(this.cardState.author_name?.slice(0, 2).join(', ') ?? 'Unknown author', 24)}</span>
-				</div>
+				</a>
 				<button class='card__btn ${isFavorite ? 'card__btn_favorite' : ''}'>
 					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M5 5.00001C5 4.07954 5.74619 3.33334 6.66667 3.33334H13.3333C14.2538 3.33334 15 4.07954 15 5.00001V17.5L10 12.5L5 17.5V5.00001Z" stroke="#121212" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
