3d149c128113a22377b0fc0defb14cf7e5eaf29f
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-20 22:47:51 +0300

    Fix paths finally

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,7 @@
 import postcss from 'rollup-plugin-postcss';
 import path from 'path';
+import copy from 'rollup-plugin-copy';
+import url from 'postcss-url';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
@@ -13,6 +15,21 @@ export default {
 			extract: path.resolve('dist/style.css'),
 			modules: false,
 			minimize: true,
+			plugins: [
+				url({
+					url: (asset) => {
+						if (asset.url.startsWith('/static/icons/')) {
+							return `icons/${asset.url.split('/').pop()}`;
+						}
+						return asset.url;
+					},
+				}),
+			],
+		}),
+		copy({
+			targets: [
+				{ src: 'static/icons/*', dest: 'dist/icons' },
+			],
 		}),
 		nodeResolve(),
 	],
